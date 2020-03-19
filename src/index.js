4203f728a7047c46af57850751424d5c30fb2d44
dosenk <eympel@gmail.com>
2020-03-19 14:56:33 +0300

    feat: completed the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    let num = n < 0 ? String(n).slice(1) : String(n);
+    return Number(num.split('').reverse().join(''));
 }
